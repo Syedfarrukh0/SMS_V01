c1064498eca6b279b9dc7b6ba322ba5c41943aa3
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-09-30 07:18:56 -0700

    push menu code

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className={``}> Dashboard Layout {children}</div>;
+  return <div className={`h-screen flex overflow--hidden`}>
+    {/* Left */}
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow--x-hidden overflow--scroll">
+      <Link href={'/'} className="flex justify-center lg:justify-start items-center gap-2">
+        <Image src={'/logo.png'} alt="logo" width={32} height={32} />
+        <span className="hidden lg:block"> School Suite </span>
+      </Link>
+      <Menu />
+    </div>
+    {/* Right */}
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-x-hidden overflow-scroll">
+      <Navbar />
+      {children}
+    </div>
+  </div>;
 }
