608263b3b74459c1ec6b7670de904bd6485da7a9
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-10-01 07:24:26 -0700

    push

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* left */}
+      <div className='w-full lg:w-2/3'>l</div>
+      {/* right */}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
   )
 }
 
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,19 +8,24 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className={`h-screen flex overflow--hidden`}>
-    {/* Left */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow--x-hidden overflow--scroll">
-      <Link href={'/'} className="flex justify-center lg:justify-start items-center gap-2">
-        <Image src={'/logo.png'} alt="logo" width={32} height={32} />
-        <span className="hidden lg:block"> School Suite </span>
-      </Link>
-      <Menu />
+  return (
+    <div className={`h-screen flex overflow--hidden`}>
+      {/* Left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow--x-hidden overflow--scroll">
+        <Link
+          href={"/"}
+          className="flex justify-center lg:justify-start items-center gap-2"
+        >
+          <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+          <span className="hidden lg:block"> School Suite </span>
+        </Link>
+        <Menu />
+      </div>
+      {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-x-hidden overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
-    {/* Right */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-x-hidden overflow-scroll">
-      <Navbar />
-      {children}
-    </div>
-  </div>;
+  );
 }
