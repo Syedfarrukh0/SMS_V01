d57038d1aef3ac7cf54dfb6a1125f56e5521be1f
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-10-14 03:02:34 -0700

    push chart with real data to data base pie or bar chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -5,6 +5,8 @@ import CountChart from "@/components/CountChart";
 import FinanceChart from "@/components/FinanceChart";
 import EventCalendar from "@/components/EventCalendar";
 import Announcements from "@/components/Announcements";
+import { CountChartContainer } from "@/components/CountChartContainer";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
 
 function AdminPage() {
   return (
@@ -13,20 +15,20 @@ function AdminPage() {
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* user cards */}
         <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="admin" />
+          <UserCard type="teacher" />
           <UserCard type="student" />
-          <UserCard type="student" />
-          <UserCard type="student" />
-          <UserCard type="student" />
+          <UserCard type="parent" />
         </div>
         {/* middle charts */}
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* count chart */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart />
+            <CountChartContainer />
           </div>
           {/* attendance chart */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
+            <AttendanceChartContainer />
           </div>
         </div>
         {/* bottom charts */}
