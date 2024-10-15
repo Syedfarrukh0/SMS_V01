e2c7a56ad8fb37054a097517646ade67c0fd8e5a
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-10-14 22:23:39 -0700

    push event calender

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import UserCard from "@/components/UserCard";
-import AttendanceChart from "@/components/AttendanceChart";
-import CountChart from "@/components/CountChart";
 import FinanceChart from "@/components/FinanceChart";
-import EventCalendar from "@/components/EventCalendar";
 import Announcements from "@/components/Announcements";
 import { CountChartContainer } from "@/components/CountChartContainer";
 import AttendanceChartContainer from "@/components/AttendanceChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 
-function AdminPage() {
+const AdminPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row max-lg:flex-wrap">
       {/* left */}
@@ -38,7 +40,7 @@ function AdminPage() {
       </div>
       {/* right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
