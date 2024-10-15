ca1b98df378049a813a6dac764ec36eac972433b
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-10-15 04:41:14 -0700

    push announcement page on continus working

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,6 +1,28 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-function Announcements() {
+const Announcements = async () => {
+
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as {role?:string})?.role
+
+  const roleConditions = {
+    admin: {},
+    teacher: {lessons: { some: { teacherId: userId! } }},
+    student: {students: { some: { Id: userId! } }},
+    parent: {students: { some: { parentId: userId! } }},
+  }
+
+  const data = await prisma.announcement.findMany({
+    take: 3,
+    orderBy: { date: 'desc' },
+    where: {
+      OR: [
+        {classId: null}, {class: roleConditions[role as keyof typeof roleConditions ] || {} }
+      ]
+    }
+  })
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
@@ -10,38 +32,35 @@ function Announcements() {
       <div className="flex flex-col gap-4 mt-4">
         <div className="bg-lamaSkyLight rounded-md p-4">
           <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">{data[0].title}</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span>
           </div>
           <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            sunt tempore iure reprehenderit,
+            {data[0].description}
           </p>
         </div>
         <div className="bg-lamaPurpleLight rounded-md p-4">
           <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">{data[1].title}</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span>
           </div>
           <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            sunt tempore iure reprehenderit,
+            {data[1].description}
           </p>
         </div>
         <div className="bg-lamaYellowLight rounded-md p-4">
           <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">{data[2].title}</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span>
           </div>
           <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            sunt tempore iure reprehenderit,
+            {data[2].description}
           </p>
         </div>
       </div>
